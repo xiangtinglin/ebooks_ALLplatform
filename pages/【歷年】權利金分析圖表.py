70eb5000ca080422d83e7fd5f48d70f59c93ce90
xiangtinglin <116503010+xiangtinglin@users.noreply.github.com>
2023-12-07 18:36:00 +0800

    Update 【歷年】權利金分析圖表.py

--- pages/【歷年】權利金分析圖表.py
+++ pages/【歷年】權利金分析圖表.py
@@ -80,8 +80,7 @@ if user_input:
             
             # 绘制饼图
             fig = px.pie(x, values='權利金', names='銷售地區', title='Sales Distribution by Region', 
-                         text='權利金', 
-                         hover_data=['權利金'])
+                         text='權利金')
             # 在 Streamlit 中显示 Plotly 图表
             st.plotly_chart(fig)
         else:
