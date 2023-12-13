92e12e701576f3f83ea398a431e8e2ff06bed701
xiangtinglin <116503010+xiangtinglin@users.noreply.github.com>
2023-12-13 15:39:22 +0800

    Update 【歷年】權利金分析圖表.py

--- pages/【歷年】權利金分析圖表.py
+++ pages/【歷年】權利金分析圖表.py
@@ -55,27 +55,22 @@ if user_input:
             # Create a dropdown menu for the user to select the ranking view
             selected_ranking_view = st.selectbox("選擇排名檢視", ["檢視各單位銷售排名(歷年加總)", "檢視近3年內容收益排名"])
             
+            # Create a dropdown menu for the user to select the ranking view
+            selected_ranking_view = st.selectbox("選擇排名檢視", ["檢視各單位銷售排名(歷年加總)", "檢視近3年內容收益排名"])
+            
             # Display the selected ranking view
             if selected_ranking_view == "檢視各單位銷售排名(歷年加總)":
                 total_rank = data.groupby(by=['單位名稱'])['電子書內容收益'].sum().reset_index().sort_values(by='電子書內容收益', ascending=False)
                 total_rank.index = range(1, len(total_rank)+1)
-                st.table(total_rank)  # You can use st.table to display a nicely formatted table
+                st.markdown("### 檢視各單位銷售排名(歷年加總)")
+                st.dataframe(total_rank, height=500, width=None, scroll=True)
             
             elif selected_ranking_view == "檢視近3年內容收益排名":
                 recent_3years_data = data[data['年'].isin(data['年'].unique()[-3:])]
                 recent_3years_rank = recent_3years_data.groupby(by=['單位名稱'])['電子書內容收益'].sum().reset_index().sort_values(by='電子書內容收益', ascending=False)
                 recent_3years_rank.index = range(1, len(recent_3years_rank)+1)
-                st.table(recent_3years_rank)  # You can use st.table to display a nicely formatted table
-            '''檢視各單位銷售排名(歷年加總)'''
-            total_rank = data.groupby(by=['單位名稱'])['電子書內容收益'].sum().reset_index().sort_values(by='電子書內容收益', ascending=False)
-            total_rank.index = range(1,len(total_rank)+1)
-            total_rank
-            '''檢視近3年內容收益排名'''
-            # Assuming '年' is the column representing years
-            recent_3years_data = data[data['年'].isin(data['年'].unique()[-3:])]
-            recent_3years_rank = recent_3years_data.groupby(by=['單位名稱'])['電子書內容收益'].sum().reset_index().sort_values(by='電子書內容收益', ascending=False)
-            recent_3years_rank.index = range(1,len(recent_3years_rank)+1)
-            recent_3years_rank
+                st.markdown("### 檢視近3年內容收益排名")
+                st.dataframe(recent_3years_rank, height=500, width=None, scroll=True)
             
             '''> STEP.2 匯入檔案後，輸入條件查詢'''
             NUMBERorISBN = st.text_input("輸入:合約詳編/ISBN查詢")
