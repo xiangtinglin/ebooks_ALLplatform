58e96e9067b4a1d3cc3875a8325db2d69ee9c565
xiangtinglin <116503010+xiangtinglin@users.noreply.github.com>
2023-12-13 18:01:37 +0800

    Update 【歷年】權利金分析圖表.py

--- pages/【歷年】權利金分析圖表.py
+++ pages/【歷年】權利金分析圖表.py
@@ -51,6 +51,8 @@ if user_input:
             # ------------------------------------- 【功能】第一區 ▼-------------------------------------------
             if st.button("電子書收益總覽"):
                 view_option = st.selectbox("請選擇條件", ["收起區塊(預設)", "歷年加總", "近3年"])
+            else:
+                pass
 
             # Display selected view
             if view_option == "歷年加總":
