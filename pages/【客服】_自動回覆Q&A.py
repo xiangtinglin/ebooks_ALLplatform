780a75ef008e6049e8f6258591a80dce431c14dd
xiangtinglin <116503010+xiangtinglin@users.noreply.github.com>
2023-12-21 17:01:25 +0800

    Update 【客服】_自動回覆Q&A.py

--- pages/【客服】_自動回覆Q&A.py
+++ pages/【客服】_自動回覆Q&A.py
@@ -1,51 +1,40 @@
 import streamlit as st
-import time
 
-# 初始化對話段落
-conversation = []
-
-# 輸入框
-user_input = st.text_input("輸入框:")
-
-# 推薦相關的預設關鍵字
-recommended_keywords = ["你好", "再見", "感謝"]  # 這裡可以替換成實際的相似關鍵字
-for keyword in recommended_keywords:
-    if keyword in user_input:
-        st.write(f"推薦關鍵字：{keyword}")
-
-# 模擬等待0.05秒
-time.sleep(0.05)
-
-# 顯示區
-st.sidebar.markdown("## 對話段落")
-for para in reversed(conversation):
-    st.sidebar.text(para)
-
-# 定義預設關鍵字及相應的回覆
+# 定義預設回覆的關鍵字與答案
 default_responses = {
-    "你好": "你好！有什麼我可以幫助你的嗎？",
-    "再見": "再見！歡迎隨時回來。",
-    "感謝": "不客氣，有其他問題可以問我哦。"
+    "你好": "你好！有什麼我可以幫你的嗎？",
+    "天氣": "目前天氣晴朗，氣溫攝氏25度。",
+    "最新消息": "最新消息：XXX。",
 }
 
-# 判斷是否有預設回覆
-response = None
-for key in default_responses:
-    if key in user_input:
-        response = default_responses[key]
-        break
+# 對話段落顯示區
+conversation_history = []
 
-# 更新對話段落
-if user_input:
-    conversation.append(f"使用者: {user_input}")
-    if response:
-        conversation.append(f"機器人: {response}")
+# Streamlit應用主要程式碼
+def main():
+    st.title("自動回覆機器人")
 
-        # 顯示每個字間隔0.05秒出現，不換行
-        for char in f"機器人: {response}":
-            st.write(char, end='', key='char')
-            time.sleep(0.05)
-    st.write("")  # 換行
+    # 顯示對話段落
+    st.markdown("## 對話段落")
+    for item in reversed(conversation_history):
+        st.text(item)
 
-# 模擬等待0.05秒
-time.sleep(0.05)
+    # 輸入框
+    user_input = st.text_input("請輸入訊息：")
+
+    if user_input:
+        # 將使用者輸入的訊息加入對話歷史
+        conversation_history.append(f"你：{user_input}")
+
+        # 檢查預設回覆的關鍵字
+        for keyword, response in default_responses.items():
+            if keyword in user_input:
+                # 加入機器人回覆到對話歷史
+                conversation_history.append(f"機器人：{response}")
+
+        # 清空輸入框
+        st.text_input("")
+
+# 啟動應用
+if __name__ == "__main__":
+    main()
