3e8bd1cffcb39b8d650bbd82d2341b6abcf9f604
xiangtinglin <116503010+xiangtinglin@users.noreply.github.com>
2023-12-21 15:00:36 +0800

    Update 【客服】_自動回覆Q&A.py

--- pages/【客服】_自動回覆Q&A.py
+++ pages/【客服】_自動回覆Q&A.py
@@ -1,57 +1,56 @@
+# 安裝 streamlit：pip install streamlit
+
 import streamlit as st
 import time
 
-# 設定預設回覆的關鍵字與答案
+# 預設的關鍵字和回答
 default_responses = {
     "你好": "你好！有什麼我可以幫忙的嗎？",
-    "再見": "再見，歡迎隨時回來！",
-    "感謝": "不客氣，有其他問題歡迎問我。",
+    "再見": "再見，歡迎下次再來！",
+    "感謝": "不客氣，有其他問題就問吧！",
 }
 
-# 初始化對話紀錄
+# 初始化對話歷史
 conversation_history = []
 
-# Streamlit UI
+# Streamlit 介面
 st.title("簡易自動回覆機器人")
 
 # 第一區 - 顯示框
-st.markdown("### 第一區 - 顯示框")
-
-for interaction in conversation_history:
-    st.text(f"{interaction['user']}: {interaction['question']}")
-    st.text(f"機器人: {interaction['response']}")
-    st.markdown("---")
+st.subheader("對話歷史")
+for entry in conversation_history:
+    st.text(entry)
 
 # 第二區 - 輸入框
-st.markdown("### 第二區 - 輸入框")
+st.subheader("輸入框")
 
 # 讓使用者輸入關鍵字
-user_input = st.text_input("輸入您的問題：")
+user_input = st.text_input("請輸入您的問題：")
 
-# 檢查預設回覆的關鍵字
+# 根據關鍵字給予預設答案
 for keyword, response in default_responses.items():
     if keyword in user_input:
-        st.text("機器人正在思考中...")
-        time.sleep(1)  # 模擬思考時間
+        # 顯示推薦關鍵字
+        st.text(f"建議關鍵字：{', '.join(default_responses.keys())}")
 
-        # 逐字打印回覆
+        # 模擬回覆的打字效果
+        st.text("機器人正在思考中...")
+        with st.spinner("輸入中..."):
+            time.sleep(1)
+        
+        # 將對話加入歷史
+        conversation_history.append(f"使用者：{user_input}")
+        conversation_history.append(f"機器人：{response}")
+
+        # 顯示回覆
         for char in response:
-            st.text("機器人: " + char)
+            st.text(f"機器人：{char}")
             time.sleep(0.05)
 
-        # 將對話紀錄添加到歷史
-        conversation_history.append({"user": "使用者", "question": user_input, "response": response})
-        break
+        # 將回覆加入歷史
+        conversation_history.append(f"機器人：{response}")
 
-# 如果使用者沒有輸入，則顯示提示訊息
-if not user_input:
-    st.text("請輸入問題。")
-
-# 顯示最新的對話紀錄
-st.markdown("### 最新對話紀錄")
-if conversation_history:
-    latest_interaction = conversation_history[-1]
-    st.text(f"{latest_interaction['user']}: {latest_interaction['question']}")
-    st.text(f"機器人: {latest_interaction['response']}")
-else:
-    st.text("尚無對話紀錄。")
+# 更新對話歷史
+st.text("更新對話歷史...")
+time.sleep(1)
+st.text("對話歷史已更新！")
