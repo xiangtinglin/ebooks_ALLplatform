9612768e7b91fb53a0045751fb96afb924dd8e2c
xiangtinglin <116503010+xiangtinglin@users.noreply.github.com>
2023-12-21 17:07:22 +0800

    Update 【客服】_自動回覆Q&A.py

--- pages/【客服】_自動回覆Q&A.py
+++ pages/【客服】_自動回覆Q&A.py
@@ -1,40 +1,41 @@
 import streamlit as st
 
-# 定義預設回覆的關鍵字與答案
+# 初始化對話列表
+conversation = []
+
+# 初始化預設的關鍵字和回覆
 default_responses = {
-    "你好": "你好！有什麼我可以幫你的嗎？",
-    "天氣": "目前天氣晴朗，氣溫攝氏25度。",
-    "最新消息": "最新消息：XXX。",
+    "你好": "你好！有什麼我可以幫忙的嗎？",
+    "天氣": "請問你想查詢哪個城市的天氣？",
+    "感謝": "不客氣，有其他問題歡迎隨時問我！"
 }
 
-# 對話段落顯示區
-conversation_history = []
+# Streamlit 界面
+st.title("簡易自動回覆機器人")
 
-# Streamlit應用主要程式碼
-def main():
-    st.title("自動回覆機器人")
+# 第一區-對話段落文字顯示區
+st.sidebar.title("對話段落")
+for c in conversation:
+    st.sidebar.text(c)
 
-    # 顯示對話段落
-    st.markdown("## 對話段落")
-    for item in reversed(conversation_history):
-        st.text(item)
+# 第二區-輸入框
+user_input = st.text_input("輸入框")
 
-    # 輸入框
-    user_input = st.text_input("請輸入訊息：")
+# 如果使用者輸入非空字串
+if user_input:
+    # 將使用者輸入添加到對話列表
+    conversation.append(f"使用者: {user_input}")
 
-    if user_input:
-        # 將使用者輸入的訊息加入對話歷史
-        conversation_history.append(f"你：{user_input}")
+    # 檢查預設的關鍵字
+    for keyword, response in default_responses.items():
+        if keyword in user_input:
+            # 將回覆添加到對話列表
+            conversation.append(f"機器人: {response}")
 
-        # 檢查預設回覆的關鍵字
-        for keyword, response in default_responses.items():
-            if keyword in user_input:
-                # 加入機器人回覆到對話歷史
-                conversation_history.append(f"機器人：{response}")
+    # 清空輸入框
+    st.text_input("")
 
-        # 清空輸入框
-        st.text_input("")
-
-# 啟動應用
-if __name__ == "__main__":
-    main()
+# 顯示對話段落文字
+st.sidebar.title("對話段落")
+for c in reversed(conversation):
+    st.sidebar.text(c)
