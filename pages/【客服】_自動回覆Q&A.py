a0b9edfb73c0b6344403909db9ad382f40572327
xiangtinglin <116503010+xiangtinglin@users.noreply.github.com>
2023-12-21 15:45:00 +0800

    Update 【客服】_自動回覆Q&A.py

--- pages/【客服】_自動回覆Q&A.py
+++ pages/【客服】_自動回覆Q&A.py
@@ -11,19 +11,13 @@ default_responses = {
     "感謝": "不客氣，有其他問題可以問我哦。"
 }
 
-# 頁面佈局
-st.title("簡易自動回覆機器人")
-
-# 【輸入框】
+# 輸入框
 user_input = st.text_input("輸入框:")
 
-# 當使用者輸入相關字詞時，主動推薦相似的預設關鍵字
+# 推薦相關的預設關鍵字
 recommended_keywords = [key for key in default_responses.keys() if key in user_input]
 if recommended_keywords:
     st.write(f"推薦關鍵字：{', '.join(recommended_keywords)}")
-
-# 模擬等待0.05秒
-time.sleep(0.05)
 
 # 判斷是否有預設回覆
 response = None
@@ -36,12 +30,13 @@ for key in default_responses:
 if user_input:
     conversation.append(f"使用者: {user_input}")
     if response:
-        conversation.append(f"機器人: {response}")
+        st.write(f"機器人: ", end="")
+        for char in response:
+            st.write(char, end="", key=None)
+            time.sleep(0.05)
+        st.write("")  # 換行
 
-# 【顯示區】
-st.markdown("## 對話段落")
+# 顯示區
+st.sidebar.markdown("## 對話段落")
 for para in reversed(conversation):
-    for char in para:
-        st.write(char, end="", key=f"char-{para.index(char)}")
-        time.sleep(0.05)
-    st.write("")  # 換行
+    st.sidebar.text(para)
