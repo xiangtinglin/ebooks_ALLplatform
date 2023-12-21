d85e238ba4580506bcc767f4480771a40914a0e8
xiangtinglin <116503010+xiangtinglin@users.noreply.github.com>
2023-12-21 14:27:36 +0800

    Update 【客服】_自動回覆Q&A.py

--- pages/【客服】_自動回覆Q&A.py
+++ pages/【客服】_自動回覆Q&A.py
@@ -1,34 +1,34 @@
+# 匯入所需的套件
 import streamlit as st
 
-# 預設的關鍵字和相應的回覆
+# 預設的關鍵字與答案對應表
 default_responses = {
-    "chatgpt": "您好，我是ChatGPT。有什麼我可以幫助您的?",
-    "資訊": "您可以在這裡找到有關資訊的相關內容。",
-    # 在此添加更多的預設關鍵字和回覆
+    "聊天": "您想聊什麼呢？",
+    "問候": "哈囉！您好！",
+    "機器人": "我是您的助手，有什麼我可以幫您的嗎？",
+    "感謝": "不客氣，有其他問題歡迎問我喔！"
 }
 
-def get_default_response(user_input):
-    for keyword, response in default_responses.items():
-        if keyword in user_input:
-            return response
-    return "抱歉，我無法理解您的輸入。"
-
 # Streamlit應用程式的開始
-st.title("簡易自動回覆機器人")
+def main():
+    st.title("自動回覆機器人")
 
-# 對話視窗
-st.subheader("ChatGPT 聊天對話框")
-conversation = st.text_area("對話:", height=200, max_chars=500)
+    # 輸入框
+    user_input = st.text_input("chatgpt 聊天對話框")
 
-# 使用者輸入
-user_input = st.text_input("輸入您的訊息:")
-
-# 處理使用者輸入並回覆
-if st.button("發送"):
+    # 如果使用者有輸入文字
     if user_input:
-        conversation += f"\n你: {user_input}"
-        response = get_default_response(user_input)
-        conversation += f"\nChatGPT: {response}"
+        st.text("上一句：{}".format(user_input))
 
-# 顯示對話視窗
-st.text_area("對話紀錄:", value=conversation, height=200, max_chars=500, key="output")
+        # 判斷輸入的文字是否包含任何預設的關鍵字
+        matched_responses = [response for keyword, response in default_responses.items() if keyword in user_input]
+
+        # 如果有匹配到預設的關鍵字，顯示對應的答案
+        if matched_responses:
+            st.text("機器人：{}".format(matched_responses[0]))
+        else:
+            st.text("機器人：抱歉，我不確定如何回覆這個。")
+
+# 執行Streamlit應用程式
+if __name__ == "__main__":
+    main()
