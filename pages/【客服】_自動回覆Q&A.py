ac57a4e1d49864ed080d5ff4a48cfa3d2ff9800d
xiangtinglin <116503010+xiangtinglin@users.noreply.github.com>
2023-12-21 17:12:52 +0800

    Update 【客服】_自動回覆Q&A.py

--- pages/【客服】_自動回覆Q&A.py
+++ pages/【客服】_自動回覆Q&A.py
@@ -21,16 +21,21 @@ if user_input:
 
     # 根據不同的關鍵字給予預設答案
     if "預設1" in user_input:
-        dialogue.append("機器人：這是預設1的回覆。")
+        bot_reply = "機器人：這是預設1的回覆。"
+        dialogue.append(bot_reply)
+        st.text(bot_reply)
     elif "預設2" in user_input:
-        dialogue.append("機器人：這是預設2的回覆。")
+        bot_reply = "機器人：這是預設2的回覆。"
+        dialogue.append(bot_reply)
+        st.text(bot_reply)
     elif "預設3" in user_input:
-        dialogue.append("機器人：這是預設3的回覆。")
+        bot_reply = "機器人：這是預設3的回覆。"
+        dialogue.append(bot_reply)
+        st.text(bot_reply)
     else:
-        dialogue.append("機器人：抱歉，我不太了解您的問題。")
+        bot_reply = "機器人：抱歉，我不太了解您的問題。"
+        dialogue.append(bot_reply)
+        st.text(bot_reply)
 
     # 清空輸入框
     st.text_input("")
-
-# 設定寬度，使對話框具有聊天室感覺
-st.text_area("對話區", "", height=200)
