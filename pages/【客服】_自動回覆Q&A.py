106defe481b29d2911526c749f56035d7c10cf7f
xiangtinglin <116503010+xiangtinglin@users.noreply.github.com>
2023-12-21 14:09:37 +0800

    Update 【客服】_自動回覆Q&A.py

--- pages/【客服】_自動回覆Q&A.py
+++ pages/【客服】_自動回覆Q&A.py
@@ -1,43 +1,43 @@
 import streamlit as st
 
-# 預設關鍵字&回覆。
+# 定義預設的關鍵字和相應的回答
 default_responses = {
-    "你好": "你好！有什么我可以帮助你的吗？",
-    "天气": "目前天气晴朗。",
-    "时间": "现在是下午3点。",
-    # 添加更多关键字和回答
+    "供檔": "所需內容：",
+    "天氣": "目前天氣晴朗。",
+    "時間": "現在是下午3點。",
+    # 添加更多關鍵字和回答
 }
 
 def get_default_response(user_input):
-    # 遍历所有预设关键字，如果找到匹配的就返回相应的回答
+    # 遍歷所有預設關鍵字，如果找到匹配的就返回相應的回答
     for keyword, response in default_responses.items():
         if keyword in user_input:
             return response
-    # 如果没有匹配的关键字，返回默认回答
-    return "抱歉，我不太明白你说的是什么。"
+    # 如果沒有匹配的關鍵字，返回默認回答
+    return "抱歉，我不太明白你說的是什麼。"
 
-# Streamlit 应用程序
+# Streamlit 應用程式
 def main():
-    st.title("简易自动回复机器人")
+    st.title("簡易自動回覆機器人")
 
-    # 用于记录对话的列表
+    # 用於記錄對話的列表
     conversation_history = []
 
-    # 获取用户输入
-    user_input = st.text_input("输入你的消息:")
+    # 獲取使用者輸入
+    user_input = st.text_input("輸入你的訊息:")
 
-    if st.button("发送"):
-        # 记录用户输入到对话历史
+    if st.button("發送"):
+        # 記錄使用者輸入到對話歷史
         conversation_history.append(f"你: {user_input}")
 
-        # 获取默认回复
+        # 獲取預設回覆
         default_response = get_default_response(user_input)
 
-        # 记录机器人回复到对话历史
-        conversation_history.append(f"机器人: {default_response}")
+        # 記錄機器人回覆到對話歷史
+        conversation_history.append(f"機器人: {default_response}")
 
-    # 显示对话历史
-    st.text_area("对话历史", value="\n".join(conversation_history), height=200)
+    # 顯示對話歷史
+    st.text_area("對話歷史", value="\n".join(conversation_history), height=200)
 
 if __name__ == "__main__":
     main()
