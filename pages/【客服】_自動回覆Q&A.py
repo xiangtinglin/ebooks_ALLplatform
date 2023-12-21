7813a87f10a591ebb66bcbc7fcbd00113d05ec72
xiangtinglin <116503010+xiangtinglin@users.noreply.github.com>
2023-12-21 16:56:53 +0800

    Update 【客服】_自動回覆Q&A.py

--- pages/【客服】_自動回覆Q&A.py
+++ pages/【客服】_自動回覆Q&A.py
@@ -11,13 +11,22 @@ default_responses = {
     "感謝": "不客氣，有其他問題可以問我哦。"
 }
 
-# 輸入框
-user_input = st.text_input("輸入框:")
+# 主程式
+st.title("自動回覆機器人")
 
-# 推薦相關的預設關鍵字
+# 【輸入框】
+user_input = st.text_input("輸入框:")
+input_paragraph = f"使用者: {user_input}"
+
+# 推薦相似的預設關鍵字
 recommended_keywords = [key for key in default_responses.keys() if key in user_input]
 if recommended_keywords:
     st.write(f"推薦關鍵字：{', '.join(recommended_keywords)}")
+
+# 【顯示區】
+st.sidebar.markdown("## 對話段落")
+for para in reversed(conversation):
+    st.sidebar.text(para)
 
 # 判斷是否有預設回覆
 response = None
@@ -28,14 +37,18 @@ for key in default_responses:
 
 # 更新對話段落
 if user_input:
-    conversation.append(f"使用者: {user_input}")
+    conversation.append(input_paragraph)
     if response:
-        st.write(f"機器人: ")
-        for char in response:
-            st.write(char, key=None)
+        response_paragraph = f"機器人: {response}"
+        conversation.append(response_paragraph)
+
+        # 模擬等待0.05秒，逐字顯示
+        for char in response_paragraph:
+            st.text(char)
             time.sleep(0.05)
 
-# 顯示區
-st.sidebar.markdown("## 對話段落")
-for para in reversed(conversation):
-    st.sidebar.text()
+# 清空輸入框
+st.text_input("", key="dummy_key", value="")
+
+# 模擬等待0.05秒
+time.sleep(0.05)
