9b292eaf6de54211eb7fca23e0d25a6ec8c61796
xiangtinglin <116503010+xiangtinglin@users.noreply.github.com>
2023-12-21 15:04:51 +0800

    Update 【客服】_自動回覆Q&A.py

--- pages/【客服】_自動回覆Q&A.py
+++ pages/【客服】_自動回覆Q&A.py
@@ -1,56 +1,53 @@
-# 安裝 streamlit：pip install streamlit
-
 import streamlit as st
 import time
 
-# 預設的關鍵字和回答
-default_responses = {
-    "你好": "你好！有什麼我可以幫忙的嗎？",
-    "再見": "再見，歡迎下次再來！",
-    "感謝": "不客氣，有其他問題就問吧！",
-}
-
-# 初始化對話歷史
+# 儲存對話歷史的列表
 conversation_history = []
 
-# Streamlit 介面
-st.title("簡易自動回覆機器人")
+# 預設的回覆字典，關鍵字對應回覆
+default_responses = {
+    "你好": "你好！有什麼我可以幫助你的嗎？",
+    "再見": "再見，歡迎下次再來！",
+    "感謝": "不客氣，有其他問題也可以問我哦！",
+}
+
+# Streamlit 界面配置
+st.title("自動回覆機器人")
 
 # 第一區 - 顯示框
-st.subheader("對話歷史")
+st.markdown("### 對話歷史")
 for entry in conversation_history:
     st.text(entry)
 
 # 第二區 - 輸入框
-st.subheader("輸入框")
+user_input = st.text_input("輸入你的訊息：")
 
-# 讓使用者輸入關鍵字
-user_input = st.text_input("請輸入您的問題：")
-
-# 根據關鍵字給予預設答案
+# 判斷是否有相對應的預設回覆
+matched_response = None
 for keyword, response in default_responses.items():
     if keyword in user_input:
-        # 顯示推薦關鍵字
-        st.text(f"建議關鍵字：{', '.join(default_responses.keys())}")
+        matched_response = response
+        break
 
-        # 模擬回覆的打字效果
-        st.text("機器人正在思考中...")
-        with st.spinner("輸入中..."):
-            time.sleep(1)
-        
-        # 將對話加入歷史
-        conversation_history.append(f"使用者：{user_input}")
-        conversation_history.append(f"機器人：{response}")
+# 如果有預設回覆，顯示回覆
+if matched_response:
+    st.text("機器人回覆：")
+    for char in matched_response:
+        st.text(char)
+        time.sleep(0.05)
 
-        # 顯示回覆
-        for char in response:
-            st.text(f"機器人：{char}")
-            time.sleep(0.05)
+    # 將對話加入歷史紀錄
+    conversation_history.append(f"使用者：{user_input}")
+    conversation_history.append(f"機器人：{matched_response}")
 
-        # 將回覆加入歷史
-        conversation_history.append(f"機器人：{response}")
+# 如果沒有預設回覆，不顯示回覆
+elif user_input:
+    # 將對話加入歷史紀錄
+    conversation_history.append(f"使用者：{user_input}")
 
-# 更新對話歷史
-st.text("更新對話歷史...")
-time.sleep(1)
-st.text("對話歷史已更新！")
+# 顯示清空按鈕
+if st.button("清空對話歷史"):
+    conversation_history = []
+
+# 更新 Streamlit 介面
+st.text("")
