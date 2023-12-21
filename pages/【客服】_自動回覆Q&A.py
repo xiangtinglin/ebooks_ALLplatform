eaf91fe4725a20ff7e85806511649c4f445afd3e
xiangtinglin <116503010+xiangtinglin@users.noreply.github.com>
2023-12-21 14:34:20 +0800

    Update 【客服】_自動回覆Q&A.py

--- pages/【客服】_自動回覆Q&A.py
+++ pages/【客服】_自動回覆Q&A.py
@@ -1,34 +1,47 @@
-# 匯入所需的套件
 import streamlit as st
 
-# 預設的關鍵字與答案對應表
+# 定義預設的關鍵字和對應的回答
 default_responses = {
-    "聊天": "您想聊什麼呢？",
-    "問候": "哈囉！您好！",
-    "機器人": "我是您的助手，有什麼我可以幫您的嗎？",
-    "感謝": "不客氣，有其他問題歡迎問我喔！"
+    "你好": "你好！有什麼我可以幫助你的嗎？",
+    "天氣": "天氣晴朗，適合出門活動。",
+    "新聞": "最新的新聞是......",
+    # 添加更多的預設關鍵字和回答
 }
 
-# Streamlit應用程式的開始
-def main():
-    st.title("自動回覆機器人")
+# 初始化對話紀錄
+conversation_history = []
 
-    # 輸入框
-    user_input = st.text_input("chatgpt 聊天對話框")
+# Streamlit應用程式的標題
+st.title("簡易自動回覆機器人")
 
-    # 如果使用者有輸入文字
-    if user_input:
-        st.text("上一句：{}".format(user_input))
+# 第一區-輸入框
+user_input = st.text_input("請輸入你的問題或關鍵字：")
 
-        # 判斷輸入的文字是否包含任何預設的關鍵字
-        matched_responses = [response for keyword, response in default_responses.items() if keyword in user_input]
+# 檢查是否有預設的關鍵字
+matched_keyword = None
+for keyword in default_responses.keys():
+    if keyword in user_input:
+        matched_keyword = keyword
+        break
 
-        # 如果有匹配到預設的關鍵字，顯示對應的答案
-        if matched_responses:
-            st.text("機器人：{}".format(matched_responses[0]))
-        else:
-            st.text("機器人：抱歉，我不確定如何回覆這個。")
+# 推薦相關的預設關鍵字
+if matched_keyword:
+    st.markdown(f"建議的關鍵字：{', '.join(default_responses.keys())}")
 
-# 執行Streamlit應用程式
-if __name__ == "__main__":
-    main()
+# 根據不同關鍵字給予預設的答案
+if matched_keyword:
+    response = default_responses[matched_keyword]
+    conversation_history.append({"user": user_input, "bot": response})
+else:
+    response = "我還不太懂你的問題，請再詳細說明一下。"
+    conversation_history.append({"user": user_input, "bot": response})
+
+# 第二區-顯示框
+st.subheader("對話紀錄")
+for entry in conversation_history:
+    st.text(f"你: {entry['user']}")
+    st.text(f"機器人: {entry['bot']}")
+
+# 顯示最新的回答
+st.subheader("機器人的回答")
+st.text(response)
