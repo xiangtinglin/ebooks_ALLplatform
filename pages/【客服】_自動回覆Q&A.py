66562e324d7880ea7d261ab78893b53babcec545
xiangtinglin <116503010+xiangtinglin@users.noreply.github.com>
2023-12-21 15:13:04 +0800

    Update 【客服】_自動回覆Q&A.py

--- pages/【客服】_自動回覆Q&A.py
+++ pages/【客服】_自動回覆Q&A.py
@@ -1,53 +1,52 @@
 import streamlit as st
 import time
 
-# 儲存對話歷史的列表
-conversation_history = []
+# 初始化對話列表
+conversation = []
 
-# 預設的回覆字典，關鍵字對應回覆
+# 預設回覆字典
 default_responses = {
     "你好": "你好！有什麼我可以幫助你的嗎？",
     "再見": "再見，歡迎下次再來！",
-    "感謝": "不客氣，有其他問題也可以問我哦！",
+    "感謝": "不客氣，有其他問題歡迎問我喔！"
 }
 
-# Streamlit 界面配置
+# 預設的關鍵字
+default_keywords = list(default_responses.keys())
+
+# Streamlit 介面配置
 st.title("自動回覆機器人")
 
 # 第一區 - 顯示框
-st.markdown("### 對話歷史")
-for entry in conversation_history:
-    st.text(entry)
+st.text_area("對話區域", value="\n".join(conversation), height=300, max_chars=None, key="conversation")
 
 # 第二區 - 輸入框
-user_input = st.text_input("輸入你的訊息：")
+user_input = st.text_input("請輸入您的問題：")
 
-# 判斷是否有相對應的預設回覆
-matched_response = None
+# 推薦相關關鍵字
+recommended_keywords = [keyword for keyword in default_keywords if keyword in user_input]
+if recommended_keywords:
+    st.text("推薦關鍵字：" + ", ".join(recommended_keywords))
+
+# 模擬自動回覆
+for char in "正在思考中...":
+    st.text(char)
+    time.sleep(0.05)
+
+# 根據關鍵字給予預設答案
 for keyword, response in default_responses.items():
     if keyword in user_input:
-        matched_response = response
+        reply = response
         break
+else:
+    reply = "抱歉，我不太理解您的問題。"
 
-# 如果有預設回覆，顯示回覆
-if matched_response:
-    st.text("機器人回覆：")
-    for char in matched_response:
-        st.text(char)
-        time.sleep(0.05)
+# 更新對話列表
+conversation.append(f"使用者：{user_input}")
+conversation.append(f"機器人：{reply}")
 
-    # 將對話加入歷史紀錄
-    conversation_history.append(f"使用者：{user_input}")
-    conversation_history.append(f"機器人：{matched_response}")
+# 顯示回覆
+st.text(reply)
 
-# 如果沒有預設回覆，不顯示回覆
-elif user_input:
-    # 將對話加入歷史紀錄
-    conversation_history.append(f"使用者：{user_input}")
-
-# 顯示清空按鈕
-if st.button("清空對話歷史"):
-    conversation_history = []
-
-# 更新 Streamlit 介面
-st.text("")
+# 更新對話區域
+st.text_area("對話區域", value="\n".join(conversation), height=300, max_chars=None, key="conversation")
