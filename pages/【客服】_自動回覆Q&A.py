4f207ea42f4e513c3af0cd576ac981b4125fd200
xiangtinglin <116503010+xiangtinglin@users.noreply.github.com>
2023-12-21 15:23:58 +0800

    Update 【客服】_自動回覆Q&A.py

--- pages/【客服】_自動回覆Q&A.py
+++ pages/【客服】_自動回覆Q&A.py
@@ -1,45 +1,47 @@
 import streamlit as st
 import time
 
-# 定義預設的關鍵字和相對應的答案
+# 初始化對話段落
+conversation = []
+
+# 第一區-對話段落文字顯示區
+st.sidebar.markdown("## 對話段落")
+for para in reversed(conversation):
+    st.sidebar.text(para)
+
+# 第二區-輸入框
+user_input = st.text_input("輸入框:")
+
+# 模擬等待0.05秒
+time.sleep(0.05)
+
+# 定義預設關鍵字及相應的回覆
 default_responses = {
     "你好": "你好！有什麼我可以幫助你的嗎？",
-    "天氣": "請問你想查詢哪個城市的天氣？",
-    "感謝": "不客氣，有其他問題歡迎隨時問我！",
+    "再見": "再見！歡迎隨時回來。",
+    "感謝": "不客氣，有其他問題可以問我哦。"
 }
 
-# 初始化對話歷史
-conversation_history = []
+# 第二區-輸入框
+user_input = st.text_input("輸入框:")
 
-# Streamlit UI設計
-st.title("簡易自動回覆機器人")
+# 推薦相關的預設關鍵字
+recommended_keywords = [key for key in default_responses.keys() if key in user_input]
+if recommended_keywords:
+    st.write(f"推薦關鍵字：{', '.join(recommended_keywords)}")
 
-# 顯示對話段落文字
-st.markdown("## 對話段落文字")
-for paragraph in reversed(conversation_history):
-    st.text(paragraph)
+# 判斷是否有預設回覆
+response = None
+for key in default_responses:
+    if key in user_input:
+        response = default_responses[key]
+        break
 
-# 輸入框
-user_input = st.text_input("輸入您的訊息：")
-
-# 根據用戶輸入處理回覆
+# 更新對話段落
 if user_input:
-    conversation_history.append(f"使用者: {user_input}")
-    st.text("機器人: ", end="")
-    
-    # 逐字顯示最新回覆
-    for char in default_responses.get(user_input, "抱歉，我不太理解你的問題。"):
-        st.text(char)
-        time.sleep(0.05)
-    
-    # 增加機器人回覆到對話歷史
-    conversation_history.append(f"機器人: {default_responses.get(user_input, '抱歉，我不太理解你的問題。')}")
+    conversation.append(f"使用者: {user_input}")
+    if response:
+        conversation.append(f"機器人: {response}")
 
-# 顯示輸入相關的預設關鍵字
-if user_input:
-    suggested_keywords = [keyword for keyword in default_responses.keys() if keyword in user_input]
-    if suggested_keywords:
-        st.text(f"相關關鍵字推薦: {', '.join(suggested_keywords)}")
-
-# 將對話歷史限制在10個段落以內
-conversation_history = conversation_history[-10:]
+# 模擬等待0.05秒
+time.sleep(0.05)
